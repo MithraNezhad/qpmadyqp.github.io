74ff7f6e560129cc619d86352fe2ad2cd8ce0f99
qpmadyqp <qp.mady.qp@gmail.com>
2020-05-18 16:54:07 +0200

    card

--- components/index.js
+++ components/index.js
@@ -1,12 +1,27 @@
 
-Vue.component('cards', {
-    template: '<div>This is a something</div>'
-  })
 
-  var app6 = new Vue({
-    el: '#app',
-    data: {
-      message: 'Hello Vue!'
-    }
-  })
-  
+Vue.component('cards', {
+  // The todo-item component now accepts a
+  // "prop", which is like a custom attribute.
+  // This prop is called todo.
+  props: ['card'],
+  template: `
+    <div>
+    <img src="" alt="no image">
+    <p>{{ card.text }}</p>
+</div>`
+})
+
+
+var app6 = new Vue({
+  el: '#app',
+  data: {
+    groceryList: [
+      { id: 0, text: 'Vegetables' },
+      { id: 1, text: 'Cheese' },
+      { id: 2, text: 'project2' }
+    ]
+  }
+})
+
+
