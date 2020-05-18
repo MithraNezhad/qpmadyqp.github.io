80bdd844e36b8fd58e09b92a8af9eb59fe4378be
qpmadyqp <qp.mady.qp@gmail.com>
2020-05-18 19:07:56 +0200

    feature: sidebar, navbar, cards

--- components/index.js
+++ components/index.js
@@ -7,21 +7,48 @@ Vue.component('cards', {
   props: ['card'],
   template: `
     <div>
-    <img src="" alt="no image">
-    <p>{{ card.text }}</p>
+    <img src="./assets/altimg.svg" alt="no image">
+    <p>{{ card.title }}</p>
 </div>`
 })
 
+Vue.component('sidebar', {
+  // The todo-item component now accepts a
+  // "prop", which is like a custom attribute.
+  // This prop is called todo.
+  props: ['side'],
+  template:'<li>{{ side.item }}</li>'
+    
+})
 
-var app6 = new Vue({
+Vue.component('category', {
+  // The todo-item component now accepts a
+  // "prop", which is like a custom attribute.
+  // This prop is called todo.
+  props: ['cat'],
+  template:'<div>{{ cat.name }}</div>'
+    
+})
+
+
+var app = new Vue({
   el: '#app',
   data: {
+    sitename: 'Mehrdad Hozhabri Nezhad',
+    sidebar: [
+      { item: 'about' },
+      { item: 'curriculum vitae' },
+      { item: 'imprint' }
+    ],
     groceryList: [
-      { id: 0, text: 'Vegetables' },
-      { id: 1, text: 'Cheese' },
-      { id: 2, text: 'project2' }
+      { id: 0, title: 'Vegetables' },
+      { id: 1, title: 'Cheese' },
+      { id: 3, title: 'project1' },
+      { id: 4, title: 'project2' },
+      { id: 5, title: 'project3' }
     ]
   }
 })
+
 
 
